d822336ad87de8c2095737977dcd5b7c722d9094
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2023-10-31 13:55:38 -0700

    Add files via upload

---
+++ SQL_Scripts/machine_learning_curated.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi-human-balance`.`machine_learning_curated` (
+  `user` string,
+  `x` float,
+  `y` float,
+  `z` float,
+  `timestamp` bigint,
+  `sensorreadingtime` bigint,
+  `serialnumber` string,
+  `distancefromobject` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-human-balance/step_trainer/machine_learning_curated/'
+TBLPROPERTIES ('classification' = 'json');
